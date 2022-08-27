f413bbde1d389022c4fea176c1f419bc7f208eaf
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2022-08-27 17:00:19 +0530

    DAY-1

---
+++ DuplicateElementDetection.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+int findDuplicate(vector<int>& nums) {
+        int duplicate = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            int cur = abs(nums[i]);
+            if (nums[cur] < 0) {
+                duplicate = cur;
+                break;
+            }
+            nums[cur] *= -1;
+        }
+        
+        // Restore numbers
+        for (auto& num : nums)
+            num = abs(num);
+        
+        return duplicate;
+    }
