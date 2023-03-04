6f1932f4dd8345db0190d3fc875d9688f8df6ef2
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2023-03-04 18:15:40 +0530

    HARD

---
+++ CountSubarraysWithFixedBound.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/count-subarrays-with-fixed-bounds/description/
+long long countSubarrays(vector<int>& nums, int minK, int maxK) 
+    {
+        long long answer = 0;
+        int minPosition = -1, maxPosition = -1, leftBound = -1;
+        for (int i = 0; i < nums.size(); ++i) 
+        {
+            if (nums[i] < minK || nums[i] > maxK)
+                leftBound = i;
+            if (nums[i] == minK) 
+                minPosition = i;
+            if (nums[i] == maxK)
+                maxPosition = i;
+
+            answer += max(0, min(maxPosition, minPosition) - leftBound);
+        }
+        return answer;
+    }
