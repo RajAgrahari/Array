6e13effb7a8fe1994549d2ea76328a24336997ea
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2023-04-01 09:11:07 +0530

    Easy

---
+++ MakeArrayElementsEqual.cpp
@@ -0,0 +1,6 @@
+//https://practice.geeksforgeeks.org/problems/1f05c7c12b1084f270c57566b2110967c046730d/1
+long long int minOperations(int N) 
+    {
+        long long temp = N;
+        return (temp*temp)/4;
+    }
