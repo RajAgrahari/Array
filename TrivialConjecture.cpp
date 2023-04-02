1b898f3cecc980bad52d693337b6e5ecd8c8fe16
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2023-04-02 19:15:11 +0530

    Medium

---
+++ TrivialConjecture.cpp
@@ -0,0 +1,18 @@
+//https://codeforces.com/contest/1812/problem/D
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define NOOB ios_base::sync_with_stdio(false);
+#define CODER cin.tie(NULL);
+#define SOLUTION cout.tie(NULL);
+
+int main()
+{
+    NOOB CODER SOLUTION
+    
+        ll n;
+        cin>>n;
+        cout<<0<<endl; 
+    return 0;
+}
