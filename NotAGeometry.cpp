6445ba7a705c6bc563fcaea6cd0a01cb1f673b8e
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2023-04-02 19:14:21 +0530

    Medium

---
+++ NotAGeometry.cpp
@@ -0,0 +1,17 @@
+//https://codeforces.com/contest/1812/problem/E
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define NOOB ios_base::sync_with_stdio(false);
+#define CODER cin.tie(NULL);
+#define SOLUTION cout.tie(NULL);
+
+int main()
+{
+   NOOB CODER SOLUTION
+    ll a,b,c;
+    cin>>a>>b>>c;
+    cout<<0<<endl;
+    return 0;
+}
