547f21d89a6a52b94008ff3ade39cf2e560557cf
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2023-04-04 09:30:36 +0530

    Medium

---
+++ OptimalPartitionOfString.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/optimal-partition-of-string/
+int partitionString(string s) 
+    {
+        int i = 0, ans = 1,flag = 0;
+        while(i < s.size()){
+            int val = s[i] - 'a';
+            if( flag & (1<<val) ) {
+                flag = 0;
+                ans++;
+            }
+            flag = flag | (1<<val);
+            i++;
+        }
+        return ans;
+    }
