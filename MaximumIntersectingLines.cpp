957040abe331de9b18fc3c4491ca7830a549108a
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2023-04-11 18:49:37 +0530

    Medium

---
+++ MaximumIntersectingLines.cpp
@@ -0,0 +1,39 @@
+//https://practice.geeksforgeeks.org/problems/63c232252d445a377e01cd91adfd7d1060580038/1
+int maxIntersections(vector<vector<int>> lines, int N) 
+    {
+       int slines[N]; 
+        int elines[N]; 
+        
+      
+        for(int i=0;i<N;i++)
+        {
+            slines[i] = lines[i][0];
+            elines[i] = lines[i][1];
+        }
+        
+      
+        sort(slines, slines + N);
+        sort(elines, elines + N);
+        
+        int i=0;
+        int j=0;
+        int intersections=0;
+        int finalAns=INT_MIN;
+        
+      
+        while(i<N && j<N)
+        {
+            if(slines[i] <= elines[j])
+            {
+                intersections++;
+                finalAns = max(finalAns, intersections);
+                i++;
+            }
+            else
+            {
+                intersections--;
+                j++;
+            }
+        }
+        return finalAns;
+    }
