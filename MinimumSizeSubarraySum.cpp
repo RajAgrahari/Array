b77595d953bd6627e06f75cde92796dd92392ddf
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2023-07-06 23:57:04 +0530

    Medium

---
+++ MinimumSizeSubarraySum.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/minimum-size-subarray-sum/description/
+int minSubArrayLen(int target, vector<int>& nums) {
+        int i=0;
+        int j=0;
+        int sum=0;
+        int mn=INT_MAX;
+        while(j<nums.size()){
+            sum+=nums[j];
+            while(sum>=target){
+                sum-=nums[i];
+                mn=min(j-i+1,mn);
+                i++;
+            }
+            j++;
+        }
+        if(mn==INT_MAX){
+            return 0;
+        }
+        return mn;
+    }
